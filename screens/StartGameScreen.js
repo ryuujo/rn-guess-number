0d7c327037ee979d2f96af775f22d7782be2199a
Ryan Jonathan <ryan@kyou.id>
2019-12-18 15:54:02 +0700

    Game Screen added

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -50,7 +50,11 @@ const StartGameScreen = props => {
       <Card style={styles.summaryContainer}>
         <Text>You Selected:</Text>
         <NumberContainer>{selectedNumber}</NumberContainer>
-        <Button title="START GAME" color={Colors.primary} />
+        <Button
+          title="START GAME"
+          color={Colors.primary}
+          onPress={() => props.onStartGame(selectedNumber)}
+        />
       </Card>
     );
   }
